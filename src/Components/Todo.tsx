8339d636fc4b46cc7e00604ab8ba9db633c861df
Oh-shittt <verholantsewladislav@gmail.com>
2023-12-06 19:39:08 +0700

    TodoHooks

---
+++ src/Components/Todo.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const TodosGate: Todo[] = [
+
+];
+
+const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>(TodosGate);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  useEffect(() => {
+
+  }, []);
+
+  const generateRandomId = () => {
+    const min = 1;
+    const max = 900;
+    const randomId = Math.floor(Math.random() * (max - min + 1)) + min;
+    return (randomId)
+  };
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      const newTodoItem: Todo = {
+        id: todos.length + generateRandomId(),
+        text: newTodo,
+        completed: false,
+      };
+      setTodos([...todos, newTodoItem]);
+      setNewTodo('');
+    }
+  };
+
+  const toggleTodo = (id: number) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Удалить</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Добавить</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
